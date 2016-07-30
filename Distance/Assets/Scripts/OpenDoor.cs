8ba28e60fe434efd931844aff40405a5f2c6f101
Takase3962101 <takase3962101@gmail.com>
2016-07-30 19:27:26 +0900

    タイトル関連

---
+++ Distance/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+	float z = 0f;					//z軸の回転
+	public int code = 1;			//回転の方向、符号
+	public float count = 0f;		//回転時間
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//スタート画面の演出、扉の開閉
+		//本来はレイキャストで処理するが、テストのためにSpaceキーによる処理
+		if (Input.GetKey (KeyCode.Space) && count < 1.7f) {
+			transform.Rotate (new Vector3 (0, 0, z));
+			z += Time.deltaTime * code;
+			count += Time.deltaTime * 1f;
+		} else {
+			//countが一定以上になると、開閉をしなくなる
+			z += 0f;
+		}
+	}
+}
