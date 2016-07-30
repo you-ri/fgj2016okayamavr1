0cfc00e0fae9f08b99971a40236f3d5ffd609f59
katap- <raityann@gmail.com>
2016-07-30 20:24:29 +0900

    リザルトファイル

---
+++ Distance/Assets/Result/score_script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score_script : MonoBehaviour {
+
+	public GameObject score;
+	int i = 10;
+
+	// Use this for initialization
+	void Start () {
+		score.GetComponent<TextMesh>().text = i + "Pt";
+	}
+
+	// Update is called once per frame
+	void Update () {
+		score.GetComponent<TextMesh>().text = i + "Pt";
+	}
+}
---
+++ Distance/Assets/Result/value_script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class value_script : MonoBehaviour {
+
+	public GameObject value;
+	int i = 3000;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i < 1000) {
+			value.GetComponent<TextMesh> ().text = "まだまだだね";
+		} else if (i < 2000) {
+			value.GetComponent<TextMesh> ().text = "悪くはないと思う";
+		} else if (i < 3000) {
+			value.GetComponent<TextMesh> ().text = "なかなかやるね";
+		} else if (i < 4000) {
+			value.GetComponent<TextMesh> ().text = "素晴らしい！";
+		} else if (i >= 4000) {
+			value.GetComponent<TextMesh> ().text = "あなたが神か";
+		}
+	}
+}
