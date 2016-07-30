4d668a17fad27eec77db4085559229603d4bbc56
youri <you-ri@sasakingdom.com>
2016-07-30 15:53:22 +0900

    ゲームシーンの初期位置決定

--- Distance/Assets/Scripts/FallingObject.cs
+++ Distance/Assets/Scripts/FallingObject.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
-    public TextMesh text;
+public class FallingObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +10,6 @@ public class PlayerController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-        text.text = "Distance:" + transform.position.y + "m";
+	
 	}
 }
