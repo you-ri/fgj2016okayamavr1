fb476316785bbb7ab30da24dd8152aa74308b51a
youri <you-ri@sasakingdom.com>
2016-07-30 21:26:24 +0900

    シーン整理。ビルド設定

---
+++ Distance/Assets/Scripts/DependSceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+// 依存しているシーンの定義
+// 初期化時にシーンを追加で読み込む
+public class DependSceneLoader : MonoBehaviour
+{
+    public string sceneName;
+
+    void Awake ()
+    {
+        if (!SceneManager.GetSceneByName (sceneName).IsValid()) {
+            SceneManager.LoadScene (sceneName, LoadSceneMode.Additive);
+        }
+    }
+}
